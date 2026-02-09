7e9b3b6f7f372f351cd3d5cdc663f3e225441cd2
athiphat67 <athiphat.sun@dome.tu.ac.th>
2026-02-09 17:30:54 +0700

    feat: set dashboard as homepage and fix navigation links

--- myproject/baanhao_project/issues/urls.py
+++ myproject/baanhao_project/issues/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_tasks, name='all_tasks'),
+    path('all_tasks/', views.all_tasks, name='all_tasks'),
     path('complaints/', views.complaint_tasks, name='complaint_tasks'), 
     path('maintenance/', views.maintenance_tasks, name='maintenance_tasks'),
     path('create/complaint/', views.create_complaint, name='create_complaint'),

a1d71178fb32e6fc9d745a52c8dcc83b0d877afe
panifield <finonsay2004@gmail.com>
2026-02-11 23:46:49 +0700

    Update Notification-1

--- myproject/baanhao_project/notifications/urls.py
+++ myproject/baanhao_project/notifications/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.notifications_view, name='notifications'),
+    path('', views.notification_view, name='notifications'),
 ]

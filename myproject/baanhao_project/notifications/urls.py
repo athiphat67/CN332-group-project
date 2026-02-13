a42ef710014dc92eb1f723093b7160d25659cfeb
panifield <finonsay2004@gmail.com>
2026-02-13 16:54:21 +0700

    update notification dropdown

--- myproject/baanhao_project/notifications/urls.py
+++ myproject/baanhao_project/notifications/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "notifications"
+
 urlpatterns = [
-    path('', views.notification_view, name='notifications'),
+    path("", views.notification_view, name="list"),
+    path("broadcast/", views.broadcast_system, name="broadcast_system"),
 ]
